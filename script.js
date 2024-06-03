0dbd4f1db872181ab51594de7927902dfb5021b4
melianova <170462112+melianova@users.noreply.github.com>
2024-06-03 13:37:06 -0300

    Add hover effect

--- script.js
+++ script.js
@@ -1,9 +1,14 @@
 const grid = document.querySelector('.grid');
 
 const SIDE_LENGTH = 16;
+const VISITED_COLOR = '#303030';
 
 for (let i = 0; i < SIDE_LENGTH*SIDE_LENGTH; ++i) {
     const cell = document.createElement('div');
     cell.classList.add('cell');
+    cell.addEventListener('mouseenter', (e) => {
+        e.target.style.backgroundColor = VISITED_COLOR;
+    });
     grid.appendChild(cell);
 }
+
