b6ec98522301bf8f558f2df96f2169ba5ecf24f3
melianova <170462112+melianova@users.noreply.github.com>
2024-06-08 11:51:58 -0300

    Add a button that helps user to resize the grid

--- script.js
+++ script.js
@@ -1,14 +1,28 @@
 const grid = document.querySelector('.grid');
-
-const SIDE_LENGTH = 16;
+const promptButton = document.querySelector('.prompt.button');
+const DEFAULT_SIDE_LENGTH = 16;
 const VISITED_COLOR = '#303030';
 
-for (let i = 0; i < SIDE_LENGTH*SIDE_LENGTH; ++i) {
-    const cell = document.createElement('div');
-    cell.classList.add('cell');
-    cell.addEventListener('mouseenter', (e) => {
-        e.target.style.backgroundColor = VISITED_COLOR;
-    });
-    grid.appendChild(cell);
+promptButton.addEventListener('click', (e) => {
+    const newGridSideLength = prompt('Please enter the number of squares per side of the grid:');
+    if (Number.isNaN(+newGridSideLength))
+        return
+
+    grid.replaceChildren();
+    drawGridCells(+newGridSideLength);
+});
+
+function drawGridCells(gridSideLength) {
+    for (let i = 0; i < gridSideLength*gridSideLength; ++i) {
+        const cell = document.createElement('div');
+        const cellSize = 100 / gridSideLength;
+        cell.classList.add('cell');
+        cell.style.flex = `0 0 ${cellSize}%`;
+        cell.addEventListener('mouseenter', (e) => {
+            e.target.style.backgroundColor = VISITED_COLOR;
+        });
+        grid.appendChild(cell);
+    }
 }
 
+drawGridCells(DEFAULT_SIDE_LENGTH);
